a2d4f5f26e510e5407512bcf4e7cf07b28b1143b
anirudhlohi <74246267+anirudhlohi@users.noreply.github.com>
2021-03-11 13:10:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+
+
+var canvas = new fabric.Canvas("myCanvas")
+var block_width = 30
+var block_height = 30
+var player_x = 10
+var player_y = 10
+var object_player = ""
+var block_img = "";
+
+function player_update(){
+    fabric.Image.fromURL("player.png" , function(Img) {
+        player_object = Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+             top:player_y,
+             left:player_x
+        })
+        })
+}
+
+
+    function block_draw(block){
+        fabric.Image.fromURL(block , function(Img){
+        block_object = Img;
+        block_object.scaleToWidth(block_width);
+        block_object.scaleToHeight(block_height);
+        block_object.set({
+            top: player_y,
+            left:player_x
+            
+        
+        })
+        canvas.add(block_object);
+        })
+    }
+    window.addEventListener("keydown", my_keydown);
+    function my_keydown(e){
+var key_pressed = e.keyCode;
+console.log(key_pressed);
+if (e.shiftKey == true && key_pressed == '80'){
+    block_width = block_width + 10;
+    block_height = block_height + 10;
+    document.getElementById("current_width").innerHTML = block_width;
+    document.getElementById("current_height").innerHTML = block_height;
+}
+if (e.shiftKey == true && key_pressed == '77'){
+    block_height = block_height - 10;
+    block_width = block_width - 10;
+    document.getElementById("current_width").innerHTML = block_width;
+    document.getElementById("current_height").innerHTML = block_height;
+}
+if(key_pressed == '70'){
+    block_draw('ironman_face.png');
+    console.log("f");
+}
+if(key_pressed == '66'){
+    block_draw('spiderman_body.png');
+    console.log("b");
+}
+if(key_pressed == '76'){
+  block_draw('hulk_legs.png');
+  console.log('l');
+}
+if(key_pressed == '82'){
+    block_draw('thor_right_hand.png');
+    console.log("r")
+}
+if(key_pressed == "72"){
+    block_draw('captain_america_left_hand.png');
+    console.log("h");
+}
+if(key_pressed == '38'){
+    //up()
+    console.log("up");
+}
+if(key_pressed == '40'){
+    //down()
+    console.log("down");
+}
+if(key_pressed == '37'){
+    //left()
+    console.log("left");
+}
+if(key_pressed == '39'){
+    //right()
+    console.log("right")
+}
+    }
